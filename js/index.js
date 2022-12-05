cf84e43e0a77f3a3e7e9c2d265ba42537be685d2
mancku <adria.lopez.ortega@gmail.com>
2022-12-05 13:00:54 +0100

    Solved in JS

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import { solvePuzzle as solvePuzzleDay1 } from "./day01Puzzles1And2.js";
 import { solvePuzzle as solvePuzzleDay2 } from "./day02Puzzles1And2.js";
 import { solvePuzzle as solvePuzzleDay3 } from "./day03Puzzles1And2.js";
+import { solvePuzzle as solvePuzzleDay4 } from "./day04Puzzles1And2.js";
 
 solvePuzzleDay1();
 solvePuzzleDay2();
 solvePuzzleDay3();
+solvePuzzleDay4();
